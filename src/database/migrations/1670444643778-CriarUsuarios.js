ba89312b097eccf4ea7dc8a72473ccd4ed866f28
LuizCowBTF <luizcowbtf@gmail.com>
2022-12-08 10:59:09 -0300

    cracao database sqlite

---
+++ src/database/migrations/1670444643778-CriarUsuarios.js
@@ -0,0 +1,35 @@
+import { Table } from "typeorm";
+
+export class CriarUsuarios1635635770868 {
+
+    async up(queryRunner) {
+        await queryRunner.createTable(
+            new Table({
+                name: 'usuarios',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'string',
+                        isPrimary: true,
+                        isNullable: false
+                    },
+                    {
+                        name: 'nome',
+                        type: 'string',
+                        isNullable: false
+                    },
+                    {
+                        name: 'email',
+                        type: 'string',
+                        isNullable: true
+                    }
+                ]
+            })
+        );
+    }
+
+    async down(queryRunner) {
+        await queryRunner.dropTable('usuarios')
+    }
+
+}

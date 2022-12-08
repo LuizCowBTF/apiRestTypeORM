89d7d1c74d11265c0d7f6a5beab123743178737f
LuizCowBTF <luizcowbtf@gmail.com>
2022-12-08 10:57:59 -0300

    alteracoes na aplicacao

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('SERVE');
+import 'reflect-metadata';
+import express from 'express';
+import { router } from './routes';
+import createConnection from "./database";
+
+createConnection();
+
+// CONFIGURAÇÃO DA APLICAÇÃO
+const server = express();
+
+// CONFIGURAÇÃO DE ROTAS
+server.use(express.json());
+server.use(router);
+
+
+// CONFIGURAÇÃO E INICIALIZAÇÃO DO SERVIDOR
+server.listen(5000, () => {
+   console.log('SERVER on port 5000');
+});
